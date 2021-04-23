bcecef1c9d10585facf2abd336b961b1fcca92a5
Hadi Abaza <hadiabaza@Hadis-MacBook-Pro.local>
2021-04-22 21:31:06 -0400

    save counter on local storage

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // get target elements
-
 const incrementButton = document.querySelector("#increment");
 const decrementButton = document.querySelector("#decrement");
 const counterElement = document.querySelector("#counter");
+// initialize counter
 //when plus button on click, increment
+if (localStorage.getItem("count") === null) {
+  localStorage.setItem("count", 0);
+}
+counterElement.innerText = localStorage.getItem("count");
 
 incrementButton.onclick = function () {
   counterElement.innerText = parseInt(counterElement.innerText) + 1;
+  localStorage.setItem("count", counterElement.innerText);
 };
 
 decrementButton.onclick = function () {
   counterElement.innerText = parseInt(counterElement.innerText) - 1;
+  localStorage.setItem("count", counterElement.innerText);
 };
